6055d5ec82fcb3061b3887ec42e3b9e540415d9d
Highpurity <development@highpurity.cc>
2023-07-12 10:08:37 -0400

    progress

--- variables.py
+++ variables.py
@@ -1,8 +1,8 @@
 # variable = container for a value. Same thing - it's not changing
 
-#name = "Robert"
-#print(type(name)) //how to check the datatype of a variable
-#print("Hello " + name)
+# name = "Robert"
+# print(type(name)) //how to check the datatype of a variable
+# print("Hello " + name)
 
 first_name = "Robert"
 last_name = "Burdsall"
@@ -10,3 +10,21 @@ full_name = first_name + " " + last_name
 print("Hello, " + full_name)
 
 age = 16
+# age = age + 1;
+age += 1
+print(age)
+print(type(age))
+print("Hello, " + str(age))
+
+height = 250.5
+print(height)
+print(type(height))
+print("Your height is: " + str(height) + "cm")
+
+
+human = False
+print(human)
+print(type(human))
+print("the value of human is: " + str(human))
+
+# all of this stuff carrys over from java & javascript
