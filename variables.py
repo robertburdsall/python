aa8fa56691d817d69bbe9cbbc9dfe20fc11e2d9a
Highpurity <development@highpurity.cc>
2023-07-12 10:01:23 -0400

    progress

---
+++ variables.py
@@ -0,0 +1,12 @@
+# variable = container for a value. Same thing - it's not changing
+
+#name = "Robert"
+#print(type(name)) //how to check the datatype of a variable
+#print("Hello " + name)
+
+first_name = "Robert"
+last_name = "Burdsall"
+full_name = first_name + " " + last_name
+print("Hello, " + full_name)
+
+age = 16
