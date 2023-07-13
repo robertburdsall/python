11a71f0e5e04eb3e73aa26d46b861757b8c3929e
Highpurity <development@highpurity.cc>
2023-07-13 14:08:25 -0400

    progress

---
+++ stringmethods.py
@@ -0,0 +1,23 @@
+name = "Robert"
+
+print(len(name))  # prints the length of the string
+
+print(name.find("o"))  # prints the location of the character within a string
+
+print(name.capitalize())  # capitalizes the first letter of the string
+
+print(name.upper())  # makes the whole string uppercase
+
+print(name.lower())  # makes the whole string lowercase
+
+print(name.isdigit())  # prints a boolean if the string is all numbers or not
+
+print(name.isalpha())  # prints if a string has all alphabetical characters or not
+
+print(name.count)  # prints how many characters are in a string
+
+print(name.replace("o", "a"))  # replaces a specific character (the first one specified) with another specific
+# character all throughout the string
+
+print(name*3)   # put *x to put your string x times
+
