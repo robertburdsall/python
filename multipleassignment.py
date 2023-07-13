fff97b51133ade40ae6c226120e3524b34716edc
Highpurity <development@highpurity.cc>
2023-07-13 14:03:10 -0400

    progress

---
+++ multipleassignment.py
@@ -0,0 +1,16 @@
+#name = "Robert"
+#age = 16
+#attractive = False
+
+name, age, attractive = "Robert", 16, False
+
+print(name)
+print(age)
+print(attractive)
+
+Spongebob, Patrick, Sandy, Squidward = 30, 30, 30, 30
+
+print(Spongebob)
+print(Patrick)
+print(Sandy)
+print(Squidward)
