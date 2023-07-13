3b93ebede0edc0938ba62ae5e859e91128fc2b12
robertburdsall <development@robertburdsall.lol>
2023-07-13 15:44:47 -0400

    progress

---
+++ 2dlists.py
@@ -0,0 +1,9 @@
+#  2D lists = a list of lists
+
+drinks = ["coffee", "soda", "tea"]
+dinner = ["pizza", "hamburger", "hotdog"]
+dessert = ["cake", "ice cream"]
+
+food = [drinks, dinner, dessert]
+
+print(food[0][1])  # the first [] refers to which array is being selected, and the second goes into the list itself
