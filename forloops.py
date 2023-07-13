42ad540ec577772a309015e7ffe1c24037958c14
robertburdsall <development@robertburdsall.lol>
2023-07-13 15:24:37 -0400

    progress

--- forloops.py
+++ forloops.py
@@ -1,7 +1,19 @@
+import time
 #  for loop = a statement that will execute it's block of code a limited amount of times
 
 #  while loop = unlimited
 #  for loop = potentially unlimited
 
-for i in range(10):
-    print(i+1)
+# for i in range(10):
+#    print(i+1)
+
+#for i in range(50, 101, 3):  # arg 3 is the step
+#    print(i)
+
+#for i in "Robert Burdsall":
+#    print(i)
+
+for seconds in range(10, 0, -1):
+    print(seconds)
+    time.sleep(1)
+print("Happy new year!!!")
