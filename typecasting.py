f9311edf778dad82b51a7a48cbf394514f21d5a6
Highpurity <development@highpurity.cc>
2023-07-13 14:18:17 -0400

    progress

---
+++ typecasting.py
@@ -0,0 +1,15 @@
+# type casting = converting the data type of one value to another data type
+
+x = 1  # int
+y = 2.0  # float
+z = "3"  # str
+
+
+x = float(x)
+y = int(y)  # makes the float into an int
+z = int(z)
+
+print(x)
+print(y)
+# or you could just do print(int(y))
+print(z*3)
