b5da7b5c4a78b99b6807770d26a782af101069d3
robertburdsall <development@robertburdsall.lol>
2023-07-13 15:48:25 -0400

    progress

---
+++ tuples.py
@@ -0,0 +1,13 @@
+#  tuple = a collection of elements which is ordered and unchangeable
+#  used to group together related data
+
+student = ("Bro", 21, "male")
+
+student.count("Bro")  # prints the amount of times something is present in a tuple
+student.index("male")  # prints the index in which an element is present at
+
+for x in student:
+    print(x, end=" ")
+
+if "Bro" in student:
+    print("Bro is here!")
