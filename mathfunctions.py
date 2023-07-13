20be6b152bdbfb4aeb943f5486a11dc09e788411
Highpurity <development@highpurity.cc>
2023-07-13 14:31:03 -0400

    progress

---
+++ mathfunctions.py
@@ -0,0 +1,16 @@
+import math
+
+pi = 3.14
+
+x = 1
+y = 2
+z = 3
+
+print(round(pi))  # rounds a number normally
+print(math.ceil(pi))  # rounds a number up
+print(math.floor(pi))  # rounds a number down
+print(abs(pi))  # gives you the absolute value of a number
+print(pow(pi, 2))  # raise a number to an exponent (power)
+print(math.sqrt(pi))  # get the square root of a number
+print(max(x, y, z))  # get the largest number of a varying amount of numbers
+print(min(x, y, z)) # gets the smallest number of a varying amount of numbers
