8eb73c0f97503a9b8a05ed9abb12169d781ce517
robertburdsall <development@robertburdsall.lol>
2023-07-13 15:20:24 -0400

    progress

---
+++ forloops.py
@@ -0,0 +1,7 @@
+#  for loop = a statement that will execute it's block of code a limited amount of times
+
+#  while loop = unlimited
+#  for loop = potentially unlimited
+
+for i in range(10):
+    print(i+1)
